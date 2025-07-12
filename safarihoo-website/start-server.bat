f2c2a71d37df1948c3176508a1a60e86a1d1adad
hamedmak <97686430+hamedmak@users.noreply.github.com>
2025-07-12 17:21:02 +0100

    Delete safarihoo-website directory

--- safarihoo-website/start-server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Safarihoo Website Development Server...
-echo.
-echo The website will be available at: http://localhost:8000
-echo Press Ctrl+C to stop the server
-echo.
-python -m http.server 8000
-pause 
